12d8b709681e95fd7a5b476fee0ea8e7794c00e2
Yuriy <umz38@yandex.ru>
2020-03-05 15:44:03 +0300

    finished html generation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var images = [
+var images_names = [
     "donut.jpg",
     "donut.jpg",
     "donut.jpg",
@@ -6,20 +6,19 @@ var images = [
     "donut.jpg"
 ];
 
-var pics = [];
-var number = 10;
-var length = 100/number;
+var images = [];
+var length = 100/images_names.length;
 var picdimension = length * 18 / 20;
 var offset = length - picdimension;
 
-for(var i = 0; i < images.length; ++i){
+for(var i = 0; i < images_names.length; ++i){
     var elem = document.createElement('img');
     elem.style.width = picdimension.toString() + "vw";
     elem.style.height = picdimension.toString() + "vw";
     elem.style.top = offset.toString() + "vw";
     elem.style.position = "absolute";
     elem.style.left = (offset + length * i).toString() + "vw";
-    elem.src = images[i];
+    elem.src = images_names[i];
     document.body.append(elem);
-    pics.push(elem);
+    images.push(elem);
 }
