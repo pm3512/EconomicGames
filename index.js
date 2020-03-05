c1ea1665dad1d55a87b810cf7d16c81df4a842c5
Yuriy <umz38@yandex.ru>
2020-03-05 15:31:58 +0300

    Create donuts array

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world");
+var pics = [];
+for(var i = 0; i < 5; ++i){
+    var elem = document.createElement('img');
+    elem.src = "donut.jpg";
+    elem.style.width = "18vw";
+    elem.style.height = "18vw";
+    elem.style.top = "1vw";
+    elem.style.position = "absolute";
+    elem.style.left = (1 + 20 * i).toString() + "vw";
+    document.body.append(elem);
+    pics.push(elem);
+}
