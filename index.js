2ea65d763e5d670e91a228095721642e7860774a
Yuriy <umz38@yandex.ru>
2020-03-06 12:37:37 +0300

    push game elems to div instead of body

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ var length = 100/images_names.length;
 var picdimension = length * 18 / 20;
 var offset = length - picdimension;
 
+let game = document.getElemenentById('game');
+
 for(var i = 0; i < images_names.length; ++i){
-    var elem = document.createElement('img');
+    var elem = document.createElement("img");
     elem.style.width = picdimension.toString() + "vw";
     elem.style.height = picdimension.toString() + "vw";
     elem.style.top = offset.toString() + "vw";
@@ -20,6 +22,21 @@ for(var i = 0; i < images_names.length; ++i){
     elem.style.left = (offset + length * i).toString() + "vw";
     elem.src = images_names[i];
     elem.id = "image_" + i;
-    document.body.append(elem);
+    game.append(elem);
     images.push(elem);
 }
+
+var button = document.createElement("ready");
+button.style.position = "absolute";
+button.style.width = picdimension.toString() + "vw";
+button.style.height = (picdimension / 3).toString() + "vw";
+button.style.top = length.toString() + "vw";
+button.style.left =(offset + length * ((images_names.length - 1) / 2)).toString()  + "vw";
+game.append(button);
+var button_text = document.createElement("text");
+button_text.innerText = "ready";
+button_text.style.textAlign = "center";
+button_text.style.display = "table-cell";
+button_text.style.verticalAlign = "middle";
+button_text.style.position = "relative";
+button.appendChild(button_text);
