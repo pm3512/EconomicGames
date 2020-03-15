bdc1da4dbd3b59c9f67a363ca47d3f570494ad15
updog123 <obol03al@gmail.com>
2020-03-15 18:09:21 +0300

    fixed images

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
+var left = document.getElementById("left");
+var right = document.getElementById("right");
+
+left.style.paddingLeft ="10vw";
+right.style.float = "right";
+right.style.paddingRight = "10vw";
+
 var iframe = document.getElementById("myFrame");
 
-set_iframe_size_style();
+set_element_size_style();
 
 iframe.style.position = "relative";
 iframe.style.width = "80vw";
 iframe.style.left = "10vw";
 
-function set_iframe_size_style() {
+function set_element_size_style() {
     var w = document.documentElement.clientWidth;
     var h = document.documentElement.clientHeight;
-    if (h > (200 / 0.6)) {
+    if (h > 200 / 0.6) {
         iframe.style.height = "60vh";
     } else {
         iframe.style.height = "200px";
@@ -19,7 +26,14 @@ function set_iframe_size_style() {
     } else {
         iframe.style.width = "640px";
     }
+    if(w > 200 / 0.4) {
+        left.style.width = "30vw";
+        right.style.width = "30vw";
+    } else {
+        left.style.width = "200px";
+        right.style.width = "200px";
+    }
 }
 
-window.addEventListener("resize", set_iframe_size_style);
+window.addEventListener("resize", set_element_size_style);
 localStorage.clear();
