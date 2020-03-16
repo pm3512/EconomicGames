36cb3edb51a0917b9c3614fe18b81c419e29f71b
Iurii Zamiatin <umz38@yandex.ru>
2020-03-16 14:32:32 +0300

    Fixed image resision for cases when < 200px

--- index.js
+++ index.js
@@ -15,19 +15,6 @@ iframe.style.height = "30vw";
 iframe.style.left = "10vw";
 
 function set_element_size_style() {
-    var w = document.documentElement.clientWidth;
-    var h = document.documentElement.clientHeight;
-    if (w > 200 / 0.3) {
-        iframe.style.height = "30vw";
-    } else {
-        iframe.style.height = "200px";
-    }
-    if (w > 640 / 0.8) {
-        iframe.style.width = "80vw";
-    } else {
-        iframe.style.width = "640px";
-    }
-        
     left.style.width = "30vw";
     right.style.width = "30vw";
 }
